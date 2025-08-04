66b93e71b1d58e28a71cf0d240749f7a48f184d6
nansonchin <35142019+nansonchin@users.noreply.github.com>
2025-08-04 12:21:55 +0800

    update

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router'
+import ScoreCircle from './ScoreCircle'
+
+const ResumeCard = ({resume:{id,companyName,jobTitle,feedback,imagePath }}: {resume:Resume}) => {
+
+    return (
+        <Link to={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000'>
+            <div className='resume-card-header'>
+                <div className='flex flex-col gap-2'>
+                    <h2 className="!text-black font-old berak-words">
+                        {companyName}
+                    </h2>
+                    <h3 className='text-lg break-words tex-gray-500'>
+                        {jobTitle}
+                        
+                    </h3>
+                </div>
+                <div className='flex-shink-0'>
+                    <ScoreCircle score={feedback.overallScore}/>
+                </div>
+            </div>
+            <div className='gradient-border aniamte-in fade-in durarion-1000'>
+                <div className='w-full h-full'>
+                    <img src={imagePath} alt="resume" className='w-full h-[350px] max-sm:h-[200px] object-cover object-top'/>
+                </div>
+            </div>
+           
+        </Link>
+    )
+}
+
+export default ResumeCard
+
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import Navbar from "~/components/navbar"
+import type { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+import ResumeCard from "~/components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume AI" },
+    { name: "description", content: "Feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar/>
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Rating</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+
+        </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume:Resume)=>(
+            <ResumeCard key={resume.id} resume={resume}/>
+          ))}
+        </div>
+      )}
+      </section>
+
+ 
+  </main>
 }
